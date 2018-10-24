026e0ae45c3ad23a3fb04b4382a4656175c508a2
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2018-10-23 22:55:24 -0300

    refactory

--- routes/addresses.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-var addresses = require('../controllers/addresses')
-
-router.route('/:address')
-    .get(addresses.get)
-
-module.exports = router
