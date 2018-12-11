1aa4362b547f098b7cd4a7235c9b35de0b7ef6d7
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2018-12-11 10:24:55 -0300

    Enable CORS

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
+    next();
+})
+
 client.connect(error => {
     const collection = client.db(config.db.name).collection('polygons')
 
